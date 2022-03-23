6d8f6ee258513d070636c2d71190a9682e3aa79c
anna-liang <annaliang56@gmail.com>
2022-03-23 14:34:40 -0400

    Changed conversation model

--- server/messenger_backend/models/conversation.py
+++ server/messenger_backend/models/conversation.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models import Q
+from django.contrib.postgres.fields import HStoreField
 
 from . import utils
 from .user import User
@@ -7,22 +8,18 @@ from .user import User
 
 class Conversation(utils.CustomModel):
 
-    user1 = models.ForeignKey(
-        User, on_delete=models.CASCADE, db_column="user1Id", related_name="+"
-    )
-    user2 = models.ForeignKey(
-        User, on_delete=models.CASCADE, db_column="user2Id", related_name="+", 
-    )
+    users = models.ManyToManyField(User, related_name="user_conversations")
+    unreadCounts = HStoreField()
     createdAt = models.DateTimeField(auto_now_add=True, db_index=True)
     updatedAt = models.DateTimeField(auto_now=True)
 
-    # find conversation given two user Ids
-    def find_conversation(user1Id, user2Id):
+    # find conversation given list of user Ids
+    def find_conversation(users):
         # return conversation or None if it doesn't exist
         try:
-            return Conversation.objects.get(
-                (Q(user1__id=user1Id) | Q(user1__id=user2Id)),
-                (Q(user2__id=user1Id) | Q(user2__id=user2Id)),
-            )
+            userIds = []
+            for user in users:
+                userIds.append(user.id)
+            return Conversation.objects.get(users__id=userIds)
         except Conversation.DoesNotExist:
             return None
